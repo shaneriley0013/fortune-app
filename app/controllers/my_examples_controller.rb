8ceb96013fb3ea36721fcd648a3debe89af1f0f1
SHANE RILEY <shaneriley0013@gmail.com>
2023-03-27 15:14:24 -0500

    hardcode file again

--- app/controllers/my_examples_controller.rb
+++ app/controllers/my_examples_controller.rb
@@ -2,13 +2,20 @@ class MyExamplesController < ApplicationController
   
   
   def random_fortune
-    render json: {message: "hello"}  
+    # render json: {message: "hello"}  
     fortunes = ["you will be wise", "you will be fortunate", "you will learn how to predict the future"]    
     render json: {fortune: fortunes.sample}  
   end
   
-  
-  
+  def number
+    winning_numbers =[]
+    6.times do
+      winning_numbers << rand(1..60)
+    end
+    p winning
+      # rende
+      r json: {message: "hello"}  
+
   
   
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,26 @@
 Rails.application.routes.draw do
   get "/fortune", controller: "my_examples", action: "random_fortune"
   
+  get "/lottery", controller: "my_examples", action: "lottery_number"
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
-end
